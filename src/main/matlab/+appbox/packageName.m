12f4c730a0f78f642d2a56df6156501a8e9024b3
Mark Cafaro <mark.cafaro@gmail.com>
2015-12-11 12:49:59 -0800

    Added packageName

---
+++ src/main/matlab/+appbox/packageName.m
@@ -0,0 +1,16 @@
+% Gets the package name given a path.
+% \doc\+appbox\+test\Wow.m => appbox.test
+
+function [name, className, parentPath] = packageName(path)
+    if isempty(path)
+        name = [];
+        className = [];
+        parentPath = [];
+        return;
+    end
+    [parentPath, name] = strtok(path, '+');
+    [name, className] = fileparts(name);
+    name = regexp(name, '\+(\w)+', 'tokens');
+    name = strcat([name{:}], [repmat({'.'},1,numel(name)-1) {''}]);
+    name = [name{:}];
+end
