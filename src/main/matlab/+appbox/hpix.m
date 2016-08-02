45e5fef48433386aa10b30d2e3adb589a4f2a1df
Mark Cafaro <mark.cafaro@gmail.com>
2016-08-02 10:00:52 -0700

    Improved hpix and vpix

--- src/main/matlab/+appbox/hpix.m
+++ src/main/matlab/+appbox/hpix.m
@@ -1,28 +1,20 @@
-function p = hpix(c, fig)
+function p = hpix(c)
     % Converts horizontal "character" units to pixel units. Similar to using MATLAB's built-in "character" units, 
     % but works with the default Java font instead of the MATLAB defined system font. This function is useful for 
-    % cross-platform GUIs that should scale based on the system font size. Fig only needs to be provided on the first
-    % call and must be a displayed figure. 
+    % cross-platform GUIs that should scale based on the system font size.
     %
     % Per MATLAB's docs: Character units are characters using the default system font; the width of one character is 
     % the width of the letter x, the height of one character is the distance between the baselines of two lines of text.
     
     persistent width;
     if isempty(width)
-        if nargin < 2
-            fig = gcf;
-        end
-        warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
-        jf = get(handle(fig), 'JavaFrame');
-        warning('on','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
-        vis = fig.Visible;
-        fig.Visible = 'on';
-        drawnow();
-        fig.Visible = vis;
-        graphics = jf.fHG2Client.getGraphics;
+        jf = javax.swing.JFrame();
+        jf.pack();
+        graphics = jf.getGraphics;
         font = javax.swing.UIManager.getDefaults().getFont('Panel.font');
         metrics = graphics.getFontMetrics(font);
         width = metrics.charWidth('m');
+        clear('jf')
     end
     p = c * width;
 end
